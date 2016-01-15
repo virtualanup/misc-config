3d9644196968bcfdba17c92af8e4e284848f2e2a
Anup Pokhrel <virtualanup@gmail.com>
2016-01-15 14:06:44 +0545

    Added xresources and xmodmap

--- install.sh
+++ install.sh
@@ -3,5 +3,12 @@
 
 mv ~/.config/i3/config ~/.config/i3/oldconfig
 ln -s $PWD/i3/config ~/.config/i3/config
+
 mv ~/.bashrc ~/.bashrcold
 ln -s $PWD/.bashrc ~/.bashrc
+
+mv ~/.Xmodmap ~/.Xmodmapold
+ln -s $PWD/.Xmodmap ~/.Xmodmap
+
+mv ~/.Xresources ~/.Xresourcesold
+ln -s $PWD/.Xresources ~/.Xresources
--- uninstall.sh
+++ uninstall.sh
@@ -3,3 +3,5 @@
 
 mv -f ~/.config/i3/oldconfig ~/.config/i3/config
 mv -f ~/.bashrcold ~/.bashrc
+mv -f ~/.Xresourcesold ~/Xresources
+mv -f ~/.Xmodmapold ~/.Xmodmap
