18aa6d054298109b6e0c6997e7043aa57d3bb053
Anup Pokhrel <virtualanup@gmail.com>
2016-01-15 13:52:19 +0545

    Added install and uninstall scripts

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Script to install the configuration files
+
+mv ~/.config/i3/config ~/.config/i3/oldconfig
+ln -s $PWD/i3/config ~/.config/i3/config
---
+++ uninstall.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Script to uninstall the configuration files
+
+mv -f ~/.config/i3/oldconfig ~/.config/i3/config
