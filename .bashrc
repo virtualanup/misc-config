2150246b7386e414ba39406a532543f7f24ec4d4
Anup Pokhrel <virtualanup@gmail.com>
2016-01-26 22:39:45 +0545

    Added i3 status configuration

--- .bashrc
+++ .bashrc
@@ -11,8 +11,15 @@ alias vi='nvim'
 alias vim='nvim'
 alias gpush='git push origin'
 alias gpull='git pull'
+alias mkv='mkvirtualenv --python=python3'
 PS1='[\u@\h \W]\$ '
 
 VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3
 export WORKON_HOME=~/.virtualenvs
 source /usr/bin/virtualenvwrapper.sh
+
+if [ -f ~/.private-bashrc ]; then
+    source ~/.private-bashrc
+fi
+
+
--- install.sh
+++ install.sh
@@ -1,26 +1,34 @@
 #!/bin/bash
 # Script to install the configuration files
 
-if [! -f ~/.config/i3/oldconfig]; then
+if [ ! -f ~/.config/i3/oldconfig]; then
     mv ~/.config/i3/config ~/.config/i3/oldconfig
 fi
 
 ln -s $PWD/i3/config ~/.config/i3/config
 
 
-if [! -f ~/.bashrcold ]; then
+if [ ! -f ~/.i3statusold.conf ]; then
+    mv ~/.i3status.conf ~/.i3statusold.conf
+fi
+
+ln -s $PWD/i3/i3status.conf ~/.i3status.conf
+
+
+
+if [ ! -f ~/.bashrcold ]; then
     mv ~/.bashrc ~/.bashrcold
 fi
 
 ln -s $PWD/.bashrc ~/.bashrc
 
-if [! -f ~/.Xmodmapold ]; then
+if [ ! -f ~/.Xmodmapold ]; then
     mv ~/.Xmodmap ~/.Xmodmapold
 fi
 
 ln -s $PWD/.Xmodmap ~/.Xmodmap
 
-if [! -f ~/.Xresourcesold ]; then
+if [ ! -f ~/.Xresourcesold ]; then
     mv ~/.Xresources ~/.Xresourcesold
 fi
 ln -s $PWD/.Xresources ~/.Xresources
